01905592afca388185829cf945b572ce83eb2fdb
Guntars-hub <guntars.grants@gmail.com>
2021-02-22 19:00:32 +0200

    Initial Commit

---
+++ TravelHelper/TravelHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelHelperApp.swift
+//  TravelHelper
+//
+//  Created by guntars.grants on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct TravelHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
